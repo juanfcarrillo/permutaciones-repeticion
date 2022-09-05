179440a724b23e79476f49665479d36664a14c1b
juanfi444 <78522923+juanfi444@users.noreply.github.com>
2022-09-05 13:02:44 -0500

    deleted aceptar button

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import permutation, { numberPermutation } from './utils/permutation'
 
@@ -10,36 +10,52 @@ function App() {
   const handleAddButton = e => {
     e.preventDefault()
     if (newElem !== ""){
-      setElements((elements) => elements.concat(newElem))
+      const addedElem = {
+        id : (newElem.charCodeAt(0)*Math.random()),
+        content: newElem
+      }
+      setElements((elements) => elements.concat(addedElem))
       setNewElem("")
     }
   }
 
-  const handleAceptar = e => {
-    e.preventDefault()
-    const permArr = Array.from(permutation(elements))
-    setPermutatedElems(permArr)
-    setNewElem("")
+  const handleDelete = (id) => {
+    const filteredElems = elements.filter(elem => elem.id !== id)
+    setElements(filteredElems)
   }
+
+  useEffect(() => {
+    if(elements.length > 0){
+      const permArr = Array.from(permutation(elements.map(elem => elem.content)))
+      setPermutatedElems(permArr)
+      setNewElem("")
+    }
+    
+    if(elements.length == 0 ){
+      setPermutatedElems([])
+      setNewElem("")
+    }
+  }, [elements])
 
   return (
     <div className='container-all'>
       <h3>Elementos: </h3>
-      <form onSubmit={handleAddButton}className='elements-container'>
-        <div className='elements'>
-          {elements.map(elem => 
-            <div key={(elem.charCodeAt(0)*Math.random())} className='element'>
-              {elem}
-            </div>)}
+      <div className='elements'>
+        {elements.map(elem =>
+        <div key={elem.id} className='container-delete'>
+          <div className='element'>
+            {elem.content}
+          </div>
+          <button onClick={() => handleDelete(elem.id)} className='delete-button'>x</button>
         </div>
+      )}
+      </div>
+      <form onSubmit={handleAddButton} className='elements-container'>
         <input className='input' onInput={e => setNewElem(e.target.value)} value={newElem} type="text" />
         <button className='add-button' >Agregar</button>
       </form>
-      <form className='submit-container' onSubmit={handleAceptar}>
-        <button className='m-1'>Aceptar</button>
-      </form>
       <div className='p-container'>
-        {permutatedElems.length > 0 && <p>Numero de permutaciones: <strong>{numberPermutation(elements)}</strong></p>}
+        {permutatedElems.length > 0 && <p>Numero de permutaciones: <strong>{elements.length > 0 && numberPermutation(elements.map(elem => elem.content))}</strong></p>}
         <div className='permutated-container'>
             {permutatedElems.map(elem => 
               <div key={elem} className='element'>{elem}</div>
