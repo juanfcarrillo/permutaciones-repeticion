812f2c1566d9d85572d9c934034bcd9730c184fe
juanfi444 <78522923+juanfi444@users.noreply.github.com>
2022-09-05 11:52:53 -0500

    fixed permutations with repetitions

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import { useState } from 'react'
 import './App.css'
-import permutation from './utils/permutation'
+import permutation, { numberPermutation } from './utils/permutation'
 
 function App() {
   const [elements, setElements] = useState([])
   const [newElem, setNewElem] = useState("")
-  const [rValue, setRValue] = useState("")
   const [permutatedElems, setPermutatedElems] = useState([])
 
   const handleAddButton = e => {
@@ -18,19 +17,9 @@ function App() {
 
   const handleAceptar = e => {
     e.preventDefault()
-    if(rValue !== ""){
-      const numberR = Number(rValue)
-      if (numberR <= elements.length){
-        const permArr = Array.from(permutation(elements, numberR, elements.length))
-        setPermutatedElems(permArr)
-        setRValue("")
-        setNewElem("")
-      }else{
-        alert("Valor de r mayor que n")
-      }
-    }else{
-      alert("Ingrese valor en r")
-    }
+    const permArr = Array.from(permutation(elements))
+    setPermutatedElems(permArr)
+    setNewElem("")
   }
 
   return (
@@ -47,14 +36,10 @@ function App() {
         <button className='add-button' >Agregar</button>
       </form>
       <form className='submit-container' onSubmit={handleAceptar}>
-        <div className='g-1'>
-          <label htmlFor="r-input">Valor de r:</label>
-          <input onInput={(e) => setRValue(e.target.value)} value={rValue} id='r-input' className='input' type="text" />
-        </div>
         <button className='m-1'>Aceptar</button>
       </form>
       <div className='p-container'>
-        {permutatedElems.length > 0 && <p>Numero de permutaciones: <strong>{permutatedElems.length}</strong></p>}
+        {permutatedElems.length > 0 && <p>Numero de permutaciones: <strong>{numberPermutation(elements)}</strong></p>}
         <div className='permutated-container'>
             {permutatedElems.map(elem => 
               <div key={elem} className='element'>{elem}</div>
--- src/utils/permutation.js
+++ src/utils/permutation.js
@@ -1,17 +1,27 @@
-function permutation(conjElems, r, n){
-    let conj = []
-    function makePerm(permElements, elementos, r, n) {
-        if (r == 0) {
-            conj.push(elementos);
+import { factorial } from 'mathjs'
+
+function permutation (conjElems) {
+    var set =[];
+    function permute (arr, data) {
+        var cur, memo = data || [];
+        
+        for (var i = 0; i < arr.length; i++) {
+            cur = arr.splice(i, 1)[0];
+            if (arr.length === 0) set.push(memo.concat([cur]));
+            permute(arr.slice(), memo.concat([cur]));
+            arr.splice(i, 0, cur);
         }
-          else {
-            for (let i = 0; i < n; i++) {
-                makePerm(permElements, (elementos + permElements[i] + " "), (r - 1), n)
-            }
-        }
-    }
-    makePerm(conjElems, "", r, n)
-    return new Set(conj)
+        return set;
+   }
+   return new Set(permute(conjElems).map(elem => elem.join("")));
+}
+
+export function numberPermutation(permElements){
+    let hashElems = {}
+    permElements.map(elem => hashElems[elem] !== undefined ? hashElems[elem]++ : hashElems[elem] = 1)
+    const divisor = Object.values(hashElems).reduce((acc, elem) => acc*(factorial(elem)))
+    const dividend = factorial(permElements.length)
+    return dividend/divisor
 }
 
 export default permutation
